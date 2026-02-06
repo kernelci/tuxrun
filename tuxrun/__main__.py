408f06ed4411e846231c2c6b28df214d0af1bdf8
Ben Copeland <ben.copeland@linaro.org>
2026-02-06 16:47:06 +0000

    __main__: require --device when --device-dict is provided
    Users should use the new nfs-uboot or nfs-grub device types from
    tuxlava for boards not yet registered.
    
    Fixes: 988b32a ("__main__: make --device optional when --device-dict is provided")
    Signed-off-by: Ben Copeland <ben.copeland@linaro.org>

--- tuxrun/__main__.py
+++ tuxrun/__main__.py
@@ -445,18 +445,7 @@ def main() -> int:
     LOG.setLevel(logging.DEBUG if options.debug else logging.INFO)
 
     if not options.device:
-        if options.device_dict:
-            stem = options.device_dict.stem
-            for d in Device.list():
-                if d.name == stem or d.name.endswith(f"-{stem}"):
-                    options.device = d.name
-                    break
-            if not options.device:
-                parser.error(
-                    f"cannot derive --device from device-dict filename '{stem}'. "
-                    f"Please specify --device explicitly."
-                )
-        elif not (options.tuxmake or options.tuxbuild):
+        if not (options.tuxmake or options.tuxbuild):
             parser.error("argument --device is required")
 
     if options.device and not options.device_dict:
